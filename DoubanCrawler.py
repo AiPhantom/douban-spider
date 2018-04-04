fdb6ab12b45f8f9217ab2b593b6d8b6fa41ac5c4
yhlben <yinhengli@sina.com>
2018-04-04 10:44:48 +0800

    修复bug

--- DoubanCrawler.py
+++ DoubanCrawler.py
@@ -4,26 +4,17 @@ import csv
 import requests
 
 # 抓取所有地区电影，不能获取电影对应的地区，故采用循环地区方式拉取数据
-category_list=['剧情','爱情','喜剧','科幻','动作','悬疑','犯罪','恐怖','青春','励志','战争','文艺','黑色幽默','传记','情色','暴力','音乐','家庭']
 location_list=['大陆','美国','香港','台湾','日本','韩国','英国','法国','德国','意大利','西班牙','印度','泰国','俄罗斯','伊朗','加拿大','澳大利亚','爱尔兰','瑞典','巴西','丹麦']
 # 任务1:获取每个地区、每个类型页面的URL
 
 # 经分析，tags后的参数顺序与搜索结果无关
-base_url='https://movie.douban.com/tag/#/?sort=S&range=0,10&tags=电影,{}'
+base_url='https://movie.douban.com/tag/#/?sort=S&range=9,10&tags=电影,{}'
 
 def getMovieUrl(category, location,base_url=base_url):
     query_string = category+',' if len(category)>0 else ''
     query_string += location if len(location)>0 else ''
     url = base_url.format(query_string)
     return url
-
-
-
-# 任务2: 获取电影页面 HTML
-
-# html = expanddouban.getHtml(url)
-
-# print(html)
 
 # 任务3: 定义电影类
 
@@ -36,13 +27,12 @@ class Movie:
         self.info_link = info_link
         self.cover_link = cover_link
 
-
 # 任务4: 获得豆瓣电影的信息
 
 def getMovies(category='', location=''):
     movie_array=[]
     url = getMovieUrl(category,location)
-    html = expanddouban.getHtml(url)
+    html = expanddouban.getHtml(url,loadmore=True)
     soup = BeautifulSoup(html, 'html.parser')
     content_div=soup.find(class_="list-wp")
     try:  
@@ -58,14 +48,8 @@ def getMovies(category='', location=''):
     
     return movie_array
 
-
 # 任务5: 构造电影信息数据表
 my_favorite=['动作','爱情','喜剧']
-
-
-# 抓取所有的电影种类
-# for element in content_div.find_all("span"):
-#     print(element.find("span").string)
 
 # with open("movies.csv","w") as csvfile: 
 #     writer = csv.writer(csvfile)
@@ -77,12 +61,10 @@ my_favorite=['动作','爱情','喜剧']
 #     rows=[]
 #     for item in result:
 #     	rows.append([item.name,item.rate,item.location,item.category,item.info_link,item.cover_link])
-#     print(result)
 #     writer.writerows(rows)
 
 
 # 任务6: 统计电影数据
-
 
 all_list=[]
 obj_list={}
@@ -99,8 +81,6 @@ with open("movies.csv","r") as f:
         else:
             obj_list[item[1]]={item[0]:1}
 
-
-
 def objectSort(obj_list):
     f = open("./output.txt", 'w+')
     for favorite in my_favorite:
@@ -108,13 +88,16 @@ def objectSort(obj_list):
         all_number= sum([obj[key] for key in obj.keys()])
         obj_tuples= [(key,obj[key]) for key in obj.keys()]
         sorted_tuples= sorted(obj_tuples, key=lambda obj: obj[1],reverse=True)[:3]
-        index=1
-        for item in sorted_tuples:
-            print('{}类：{}. {}:{:.2%}'.format(favorite,index,item[0],item[1]/all_number), file=f)
-            index+=1
- 
-    
+        print('{}类数量排名前三的地区：{},{},{},分别占此类别电影总数的百分比：{:.2%},{:.2%},{:.2%}'.format(favorite,sorted_tuples[0][0],sorted_tuples[1][0],sorted_tuples[2][0],sorted_tuples[0][1]/all_number,sorted_tuples[1][1]/all_number,sorted_tuples[2][1]/all_number), file=f)
+
 objectSort(obj_list)
+
+
+
+
+
+
+
 
         
 
